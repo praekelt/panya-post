d87ffe0d06f0c4f886dcb37176d0cd974816271a
Shaun Sephton <shaun@praekelt.com>
2010-03-31 17:36:31 +0200

    cleanup

--- post/admin.py
+++ post/admin.py
@@ -1,20 +1,11 @@
 from copy import deepcopy
 
-from django import forms
 from django.contrib import admin
 
 from content.admin import ContentBaseAdmin
 from post.models import Post
-from ckeditor.widgets import CKEditorWidget
-
-class PostAdminForm(forms.ModelForm):
-    content = forms.CharField(widget=CKEditorWidget())
-
-    class Meta:
-        model = Post
 
 class PostAdmin(ContentBaseAdmin):
-    #form = PostAdminForm
     fieldsets = deepcopy(ContentBaseAdmin.fieldsets)
     for fieldset in fieldsets:
         if fieldset[0] == None:
