3ef826476341b19e909287b2292367b1cbe34cdd
shaunsephton <shaunsephton@gmail.com>
2010-06-14 14:09:20 +0200

    panya rename

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-post',
-    version='dev',
-    description='Django rich post app.',
-    author='Shaun Sephton',
-    author_email='shaunsephton@gmail.com',
-    url='http://github.com/praekelt/django-post',
+    name='panya-post',
+    version='0.0.1',
+    description='Panya post app.',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/panya-post',
     packages = find_packages(),
+    install_requires = [
+        'panya',
+    ],
     include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Panya",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
 )
